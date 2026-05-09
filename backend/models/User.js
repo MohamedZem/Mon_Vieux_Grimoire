bdcbf842b89146584a888c03198ecec4acd6fb31
Zemouchi Mohamed <zemouchi.moh@gmail.com>
2026-05-09 08:44:46 +0200

    modif param email

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const uniqueValidator = require('mongoose-unique-validator').default; 
 
 // Assure que chaque email est unique dans la base de données.
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true, validate: { validator: validator.isEmail, message: 'Adresse email invalide'} },
   password: { type: String, required: true }
 });
 
